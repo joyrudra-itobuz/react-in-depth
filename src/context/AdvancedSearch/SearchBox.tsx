e629c12e1c0d05c1f43aae73084a8b74f4b0e336
Joyrudra Biswas <joyrudra@itobuz.com>
2023-12-28 18:54:09 +0530

    style : search box design fixes

--- src/context/AdvancedSearch/SearchBox.tsx
+++ src/context/AdvancedSearch/SearchBox.tsx
@@ -3,8 +3,8 @@ import { CiSearch } from 'react-icons/ci';
 export default function SearchBox() {
   return (
     <div className=' z-99 fixed top-0 flex h-screen w-screen items-center justify-center bg-black/40 backdrop-blur-sm'>
-      <div className='search-box mx-5 w-full rounded-3xl bg-[#4b4b4b] p-5 lg:max-w-[50%]'>
-        <div className='m-2 flex items-center gap-2 rounded-3xl border-[3px] border-transparent bg-[#272727] px-3 focus:border-blue-700'>
+      <div className='search-box mx-5 w-full rounded-3xl bg-[#232323] p-5 lg:max-w-[50%]'>
+        <div className='flex items-center gap-2 rounded-3xl border-[3px] border-transparent bg-[#3b3b3b] px-3 focus:border-blue-700'>
           <CiSearch className='text-2xl' />
           <input
             type='text'
@@ -13,7 +13,7 @@ export default function SearchBox() {
           />
         </div>
 
-        <div className='recent-searches'>
+        <div className='recent-searches mt-5'>
           <h4>RECENT</h4>
           <ul></ul>
         </div>
